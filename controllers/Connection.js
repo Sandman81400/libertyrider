2dd134c216b21ada6edf5d614b8633f4a63934aa
Lechopier Vincent <lechopier.vincent@gmail.com>
2017-09-27 09:31:33 +0200

    add CRUD on user

--- controllers/Connection.js
+++ controllers/Connection.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+var sqlite = require('sqlite3').verbose()
 
 var connection = null;
 var db_config = null;
@@ -14,66 +14,31 @@ exports.getInstance = function () {
         throw new Error("Database Not Configured.");
     }
     if (!connection) {
-        connection = mysql.createConnection(db_config);
-        connection.connect(function (err) {
+        connection = new sqlite.Database(db_config.database, function(err){
             if (err)
                 throw err;
-            console.log("Data Base Connected!");
+            console.log("Data Base Connected!");            
         });
+        var createTable = "CREATE TABLE IF NOT EXISTS user ("
+                                + "id_user INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                + "name TEXT, "
+                                + "lastname TEXT"
+                                + ");";
+        connection.all(createTable, [], function(error, rows){
+                if(error){
+                    console.log("SQL ERROR:\n    " + error.message + "\n");
+                }
+            });
+        return connection;
+    }else{
+        return connection;
     }
-    return connection;
+    
 };
 
 // ----------------------------------------------------------------------------- escape string or array recusrively (PROTECT FROM SQL INJECTION !!! USE IT EVERYWHERE ! )
 exports.esc = function (data) {
-    if (typeof data === "object") { // if object or array
-        for (let item in data) {
-            data[item] = this.esc(data[item]);
-        }
-        return data;
-    } else {
-        return mysql.escape(data);
-    }
-
-};
-
-// ----------------------------------------------------------------------------- use filter object {{value, min, max, strict}} for add WHERE to request
-exports.addFilters = function (request, filters) {
-    let filter_size = Object.keys(filters).length;
-    let count = 0; // for not add final "AND "
-    if (filter_size >= 1) {
-        request += " WHERE ";
-        let count = 0;
-        for (let filter_name in filters) {
-            // value comparaison
-            filter_name = mysql.escape(filter_name).substr(1,filter_name.length);
-            if (filters[filter_name]["value"] && filters[filter_name]["strict"] === true) {                               // if value and use =
-                request += filter_name + " = " + mysql.escape(filters[filter_name]["value"]) + " ";
-            } else if (filters[filter_name]["value"] && filters[filter_name]["strict"] === false) {                         // if value and use LIKE
-                request += filter_name + " LIKE " + mysql.escape("%" + filters[filter_name]["value"] + "%") + " ";
-            }
-            // Interval
-            else if (filters[filter_name]["min"]) {
-                request += filter_name;
-                request += (filters[filter_name]["strict"] === true) ? " >= " : " > ";
-                request += mysql.escape(filters[filter_name]["min"]) + " ";
-                if (filters[filter_name]["max"]) {
-                    request += "AND " + filter_name;
-                    request += (filters[filter_name]["strict"] === true) ? " <= " : " < ";
-                    request += mysql.escape(filters[filter_name]["max"]) + " ";
-                }
-            } else if (filters[filter_name]["max"]) {
-                request += filter_name;
-                request += (filters[filter_name]["strict"] === true) ? " <= " : " < ";
-                request += mysql.escape(filters[filter_name]["max"]) + " ";
-            }
-
-            if (++count < filter_size) {  // skip last "AND "
-                request += "AND ";
-            }
-        }
-    }
-    return(request);
+        return "'" + data + "'";
 };
 
 exports.start = function(callback){
@@ -88,7 +53,7 @@ exports.rollback = function(callback){
 
 // ----------------------------------------------------------------------------- GET Query Results 
 exports.queryResults = function (request, callback) {
-    this.getInstance().query(request, function (error, rows, fields) {
+    this.getInstance().all(request, [],  function (error, rows) {
         if (!error) {
             callback(rows);
         } else {
--- controllers/UserService.js
+++ controllers/UserService.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 exports.getAvg = function(args, res, next) {
   /**
    * average age of last race pilots
@@ -29,66 +31,98 @@ exports.getAvg = function(args, res, next) {
     });
 }
 
-exports.createUser = function(args, res, next) {
-  /**
-   * create a User
-   * create a user from a user object, ignore id
-   *
-   * user User user to create
-   * returns User
-   **/
-  var examples = {};
-  examples['application/json'] = {
-  "name" : "aeiou",
-  "id_user" : 0,
-  "lastname" : "aeiou"
+exports.createUser = function (args, res, next) {
+    /**
+     * create a User
+     * create a user from a user object, ignore id
+     *
+     * user User_1 user to create
+     * returns inline_response_200
+     **/
+    let that = this;
+    let user = args.user.value; // Get user given as parameter
+
+    if (!user) // check if user is given
+    {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({code: 400, message: "Invalid User"}));
+        return;
+    }
+    let request = "INSERT INTO user ("
+            + "name, "
+            + "lastname) "
+            + "VALUES  ("
+            + DB.esc(user.name || null) + " , "
+            + DB.esc(user.lastname || null) + ")";
+
+    DB.queryResults(request, function (results) {
+        if (results.affectedRows === 1) {                                        // if user successfully inserted
+            that.getUserById(results.insertId, function (user) {                 // get it standard
+                res.statusCode = 201;
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(user));
+            });
+        } else {                                                                // if error occured
+            res.statusCode = 403;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({code: 403, message: results.message}));
+        }
+    });
+
+
 };
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
-}
 
-exports.deleteUser = function(args, res, next) {
-  /**
-   * delete a User
-   * delete a user from an id
-   *
-   * id Integer id of user to delete
-   * returns Boolean
-   **/
-  var examples = {};
-  examples['application/json'] = true;
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
-}
+exports.deleteUser = function (args, res, next) {
+    /**
+     * delete a User
+     * delete a user from an id
+     *
+     * id Boolean id of user to delete
+     * returns Boolean
+     **/
+    let id_user = DB.esc(args.id.value || null);
+    let request = "DELETE FROM user WHERE id_user = " + id_user;
+    let that = this;
+   
+    DB.queryResults(request, function (statement) {                     // delete user
+        if (!statement.code) {                
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(true));
 
-exports.getAll = function(args, res, next) {
-  /**
-   * get all users
-   * return an array containing all users
-   *
-   * returns List
-   **/
-  var examples = {};
-  examples['application/json'] = [ {
-  "name" : "aeiou",
-  "id_user" : 0,
-  "lastname" : "aeiou"
-} ];
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
-}
+        } else {                                                        // if error while deleting user
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({code: 400, message: statement.message}));
+        }
+    });
+};
+
+exports.getAll = function (args, res, next) {
+    /**
+     * get all users
+     * return an array containing all users
+     *
+     * filter Filter filters to restrict result (optional)
+     * returns
+     **/;
+    let that = this;
+    let req = "SELECT * FROM user";
+    DB.queryResults(req, function (result) {                                    // get users from DB
+        if (Object.keys(result).length > 0 && !result.code) {                   // if user found without SQL error
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(result));
+        } else if (result.code) {                                                  // in case of error
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({code: 400, message: result.message}));
+        } else {                                                                  // in case of no user found
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({code: 404, message: "User Not Found"}));
+        }
+    });
+};
 
 exports.getUser = function(args, res, next) {
   /**
@@ -98,18 +132,23 @@ exports.getUser = function(args, res, next) {
    * user_id Integer id of required user
    * returns User
    **/
-  var examples = {};
-  examples['application/json'] = {
-  "name" : "aeiou",
-  "id_user" : 0,
-  "lastname" : "aeiou"
-};
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
+  
+  let that = this;
+    let req = "SELECT * FROM user where id_user = " + DB.esc(args.user_id.value);
+    DB.queryResults(req, function (result) {                                    // get users from DB
+        if (Object.keys(result).length > 0 && !result.code) {                   // if user found without SQL error
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(result[0]));
+        } else if (result.code) {                                                  // in case of error
+            res.statusCode = 400;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({code: 400, message: result.message}));
+        } else {                                                                  // in case of no user found
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({code: 404, message: "User Not Found"}));
+        }
+    });
 }
 
 exports.updateUser = function(args, res, next) {
@@ -120,17 +159,22 @@ exports.updateUser = function(args, res, next) {
    * user User user to update
    * returns User
    **/
-  var examples = {};
-  examples['application/json'] = {
-  "name" : "aeiou",
-  "id_user" : 0,
-  "lastname" : "aeiou"
-};
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
+    let that = this;
+    let user = args.user.value;
+    if (!user.id_user) {                                         // check if required field are presents
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({code: 403, message: "Invalid User"}));
+    } else {  
+        let request = "UPDATE user SET "
+                + "name = " + DB.esc(user.name || null) + ", "
+                + "lastname = " + DB.esc(user.lastname || null) + " "
+                + "WHERE id_user = " + DB.esc(user.id_user);
+        DB.queryResults(request, function(result){
+            res.statusCode = 201;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(user));
+        });
+    }
 }
 
